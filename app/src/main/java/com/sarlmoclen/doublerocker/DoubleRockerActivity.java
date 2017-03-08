2a7d126fd3b3165ec9d318a24ebaf79aeec1afd2
sarlmoclen <1210575921@qq.com>
2017-03-08 16:41:18 +0800

    delete_commit

--- app/src/main/java/com/sarlmoclen/doublerocker/DoubleRockerActivity.java
+++ app/src/main/java/com/sarlmoclen/doublerocker/DoubleRockerActivity.java
@@ -12,7 +12,6 @@ public class DoubleRockerActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//test_commit
 		setContentView(R.layout.activity_double_rocker);
 	}
 
